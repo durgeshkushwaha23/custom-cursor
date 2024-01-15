9846f0982939c99f807d26082bef123bf2452755
Durgesh Kushwaha <durgeshkushwaha415@gmail.com>
2024-01-15 14:18:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+// var elemImage = document.querySelector("#elem1 img")
+
+// elem1.addEventListener("mousemove",function(dets){
+//     elemImage.style.left= dets.x +"px"
+//     elemImage.style.top= dets.y +"px"
+// })
+// elem1.addEventListener("mouseenter",function(dets){
+//   elemImage.style.opacity= 1;
+ 
+// })
+// elem1.addEventListener("mouseleave",function(dets){
+//     elemImage.style.opacity= 0;
+   
+//   })
+var elem = document.querySelectorAll(".elem");
+elem.forEach(function(val){
+
+    val.addEventListener("mouseenter",function(){
+        val.childNodes[3].style.opacity=1;
+    });
+    val.addEventListener("mouseleave",function(){
+        val.childNodes[3].style.opacity=0;
+    });
+    val.addEventListener("mousemove",function(dets){
+        val.childNodes[3].style.left=dets.x+"px"
+        val.childNodes[3].style.top=dets.y+"px"
+    });
+    
+});
+// var elem = document.querySelectorAll(".elem");
+
+// elem.forEach(function (val) {
+//     val.addEventListener("mouseenter", function () {
+//         val.childNodes[3].style.opacity = 1;
+//     });
+
+//     val.addEventListener("mouseleave", function () {
+//         val.childNodes[3].style.opacity = 0;
+//     });
+
+//     val.addEventListener("mousemove", function (dets) {
+//         var rect = val.getBoundingClientRect();
+//         var offsetX = dets.x - rect.left;
+//         var offsetY = dets.y - rect.top;
+
+//         val.childNodes[3].style.left = offsetX + "px";
+//         val.childNodes[3].style.top = offsetY + "px";
+//     });
+// });
